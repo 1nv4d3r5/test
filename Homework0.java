180f4653d427d755225240f0dfb380b9994d95fa
1nv4d3r5 <xr_417@yahoo.com>
2013-06-12 10:15:41 -0500

    Update Homework0.java

--- Homework0.java
+++ Homework0.java
@@ -1,23 +1,5 @@
-public class Homework0
-{
-    public static void main(String[] args)
-    {
-        java.util.Properties props = System.getProperties();
-        System.out.println("OS: " + props.get("os.name") 
-            + " " + props.get("os.version"));
-        System.out.println("Java: " + props.get("java.vendor") 
-            + " " + props.get("java.version"));
-        String classpath = "" + props.get("java.class.path");
-        String ide;
-        if (classpath.contains("bluej"))
-            ide = "BlueJ";
-        else if (props.get("com.horstmann.codecheck") != null)
-            ide = "Udacity";
-        else 
-            ide = "Unknown";
-        System.out.println("IDE: " + ide);
-        System.out.println("Secret code: " +
-            Math.abs(ide.hashCode() % 10000));
+public class Homework0{
+    public static void main(){
+        System.out.println("Hello world!")
     }
-}
-// This is a test
+    }
